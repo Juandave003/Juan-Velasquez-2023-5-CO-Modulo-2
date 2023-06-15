c6d2f583d95dc4797ce4c07691fe1229eae46e11
Juan Velásquez <Juan.Velasquez@fundacion-jala.org>
2023-06-14 20:36:53 -0500

    Third-Class

--- game/components/enemies/enemy.py
+++ game/components/enemies/enemy.py
@@ -2,31 +2,36 @@ import pygame
 import random
 
 from pygame.sprite import Sprite
-from game.utils.constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from game.components.bullets.bullet import Bullet
+from game.utils.constants import SCREEN_HEIGHT, SCREEN_WIDTH, ENEMY_1, ENEMY_2
 
 
 class Enemy(Sprite):
     ENEMY_WIDTH = 40
     ENEMY_HEIGHT = 60
-    X_POS_LIST = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 1000]
+    X_POS_LIST = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550]
     Y_POS = 20
     SPEED_X = 5
     SPEED_Y = 1
     MOV_X = {0: 'left', 1: 'rigth'}
+    IMAGE = {1: ENEMY_1, 2: ENEMY_2}
 
-    def __init__(self, image, width, height, x_pos_list, y_pos, speed_x, speed_y, movement_x, move_x_for):
-        self.image = pygame.transform.scale(image, (width, height))
+    def __init__(self, image = 1, speed_x = SPEED_X, speed_y = SPEED_Y, move_x_for = [30, 100]):
+        self.image = pygame.transform.scale(self.IMAGE[image], (self.ENEMY_WIDTH, self.ENEMY_HEIGHT))
         self.rect = self.image.get_rect()
-        self.rect.x = x_pos_list[random.randint(0, 10)]
-        self.rect.y = y_pos
+        self.rect.x = self.X_POS_LIST[random.randint(0, 10)]
+        self.rect.y = self.Y_POS
         self.speed_x = speed_x
         self.speed_y = speed_y
-        self.movement_x = movement_x
-        self.move_x_for = move_x_for
+        self.movement_x = self.MOV_X[random.randint(0, 1)]
+        self.move_x_for = random.randint(move_x_for[0], move_x_for[1])
         self.index = 0
+        self.type = 'enemy'
+        self.shooting_time = random.randint(30, 50)
 
-    def update(self, ships):
+    def update(self, ships, game):
         self.rect.y += self.speed_y
+        self.shoot(game.bullet_manager)
 
         if self.movement_x == 'left':
             self.rect.x -= self.speed_x
@@ -49,3 +54,10 @@ class Enemy(Sprite):
         elif (self.index >= self.move_x_for and self.movement_x == 'left') or (self.rect.x <= 10):
             self.movement_x = 'right'
             self.index = 0
+
+    def shoot(self, bullet_manager):
+        current_time = pygame.time.get_ticks()
+        if self.shooting_time <= current_time:
+            bullet = Bullet(self)
+            bullet_manager.add_bullet(bullet)
+            self.shooting_time += random.randint(30, 50)
--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -1,24 +1,30 @@
 from game.components.enemies.enemy import Enemy
-from game.utils.constants import ENEMY_1, ENEMY_2
+import random
 
 
 class EnemyManager:
     def __init__(self):
         self.enemies = []
 
-    def update(self):
+    def update(self, game):
         self.add_enemy()
 
         for enemy in self.enemies:
-            enemy.update(self.enemies)
+            enemy.update(self.enemies, game)
 
     def draw(self, screen):
         for enemy in self.enemies:
             enemy.draw(screen)
 
     def add_enemy(self):
+        enemy_type = random.randint(1, 2)
+        if enemy_type ==1:
+            enemy = Enemy()
+        else:
+            x_speed = 5
+            y_speed = 2
+            move_x_for = [1000, 1200]
+            enemy = Enemy(enemy_type, x_speed, y_speed, move_x_for)
+
         if len(self.enemies) < 1:
-            enemy1 = Enemy(ENEMY_1, 40, 60, [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 1000], 20, 5, 1, 'left', 30)
-            self.enemies.append(enemy1)
-            enemy2 = Enemy(ENEMY_2, 60, 80, [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000], 40, 3, 2, 'right', 50)
-            self.enemies.append(enemy2)
+            self.enemies.append(enemy)
